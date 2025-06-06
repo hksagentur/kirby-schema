3ed6bcad239d4f47dff5149705697181a3121580
Daniel Weidner <d.weidner@hks-agentur.de>
2025-06-06 14:25:09 +0200

    feat: track the publication/update time

---
+++ config/hooks.php
@@ -0,0 +1,64 @@
+<?php
+
+use Kirby\Cms\Page;
+use Kirby\Toolkit\A;
+
+return [
+    'page.changeStatus:after' => function (Page $newPage, Page $oldPage) {
+        $isPublished = $newPage->isPublished() && $oldPage->isDraft();
+
+        if (! $isPublished) {
+            return;
+        }
+
+        $update = A::get(
+            array: $newPage->blueprint()->field('published'),
+            key: 'update',
+            default: false,
+        );
+
+        if (! $update) {
+            return;
+        }
+
+        $newPage->update([
+            'published' => (new DateTime())->format('Y-m-d H:i:s'),
+        ]);
+    },
+    'page.create:after' => function (Page $page) {
+        $isPublished = $page->isPublished();
+
+        if (! $isPublished) {
+            return;
+        }
+
+        $update = A::get(
+            array: $page->blueprint()->field('published'),
+            key: 'update',
+            default: false,
+        );
+
+        if (! $update) {
+            return;
+        }
+
+        $page->update([
+            'published' => (new DateTime())->format('Y-m-d H:i:s'),
+        ]);
+    },
+    'page.update:after' => function (Page $newPage, Page $oldPage) {
+        $update = A::get(
+            array: $newPage->blueprint()->field('updated'),
+            key: 'update',
+            default: false,
+        );
+
+        if (! $update) {
+            return;
+        }
+
+        $newPage->update([
+            'updated' => (new DateTime())->format('Y-m-d H:i:s'),
+        ]);
+    },
+];
--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require __DIR__.'/config/helpers.php';
 Kirby::plugin('hksagentur/schema', [
     'blueprints' => require __DIR__ . '/config/blueprints.php',
     'translations' => require __DIR__ . '/config/translations.php',
+    'hooks' => require __DIR__ . '/config/hooks.php',
     'collectionMethods' => require __DIR__ . '/config/methods/collection.php',
     'fieldMethods' => require __DIR__ . '/config/methods/field.php',
 ]);
