c10c22b1287c747e8243d144bdd3b201a7df6431
Daniel Weidner <d.weidner@hks-agentur.de>
2026-02-26 16:08:10 +0100

    chore: add missing field methods

--- src/Cms/Blocks/TabsBlock.php
+++ src/Cms/Blocks/TabsBlock.php
@@ -4,9 +4,25 @@ namespace Hks\Schema\Cms\Blocks;
 
 use Kirby\Cms\Block;
 use Kirby\Cms\Structure;
+use Kirby\Content\Field;
 
 class TabsBlock extends Block
 {
+    public function level(): Field
+    {
+        return $this->content()->level()->or('h2');
+    }
+
+    public function title(): Field
+    {
+        return $this->content()->title();
+    }
+
+    public function text(): Field
+    {
+        return $this->content()->text();
+    }
+
     public function tabs(): Structure
     {
         return $this->content()->tabs()->toStructure();
